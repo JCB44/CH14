9ba40bdea60567d8505a725d0888d571ab274161
Josh <jsohua.butts4477@gmail.com>
2023-07-12 18:17:30 -0500

    user

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,4 +8,12 @@ router.get('/', async (req, res) => {
     }
   });
 
+  router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('signup');
+  });
+  
   module.exports = router;
