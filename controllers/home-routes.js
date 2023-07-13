9fc8f8f1498109a1df6195cdca9a2cad07737665
Josh <jsohua.butts4477@gmail.com>
2023-07-12 19:54:28 -0500

    login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,5 +15,9 @@ router.get('/', async (req, res) => {
     }
     res.render('signup');
   });
+
+  router.get("/login", (req, res) => {
+    res.render('login');
+  });
   
   module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+const loginUser = async (event) => {
+    event.preventDefault();
+   
+    const username = document.querySelector("#username-input-login").value.trim();
+    const password = document.querySelector("#password-input-login").value.trim();
+   
+    if (username && password) {
+      const response = await fetch("/api/user/login", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+      }); console.log(response);
+      if (response.ok) {
+        document.location.replace("/dashboard/");
+      } else {
+      }
+    }
+  };
+    
+  document.querySelector("#login-form").addEventListener("submit", loginUser);
